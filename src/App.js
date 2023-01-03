a39a9378299fb5bef81db44be86317976af15139
kishanurankar <kishanurankar@gmail.com>
2023-01-04 01:04:26 +0530

    Edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   const [book, setBook] = useState([]);
 
   const createBook = (title) => {
-    setBook(title);
+    const updatedBooks = [
+      ...book, 
+      {id: 123, title: title}
+    ]
+    setBook(updatedBooks);
     console.log("Book name is", title);
     
   };
